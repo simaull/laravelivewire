e38fad1c05226ae170ff6736dc23b789024c04d1
Muhammad Maulana <mmaulana.ulum@gmail.com>
2020-09-22 21:02:40 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Do Nothing
+#### Preview
+![](screenshot.png)
+
+Clone this repo 
+
+`composer install`
+
+setting environment variables
+
+`php artisan migrate`
+
+and ~~Do Nothing~~
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::view('data','admin.user.home');
+Route::view('/','admin.user.home');
